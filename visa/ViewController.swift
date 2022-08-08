8748f23e6e3ae2f5268c1a82b979e6de4231609c
Edgard <20133252@ealoe.ulima.edu.pe>
2022-08-08 17:59:45 -0500

    Initial Commit

---
+++ visa/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  visa
+//
+//  Created by EdgardVS on 8/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
